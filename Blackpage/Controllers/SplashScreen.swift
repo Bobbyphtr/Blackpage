68ddf4f624b4d8245a9237462dd24c3fc2c694a8
Bobbyphtr <bobbypehtrus2@gmail.com>
2019-06-24 01:42:06 +0700

    Some outro changes and optimization for splashscreen

--- Blackpage/Controllers/SplashScreen.swift
+++ Blackpage/Controllers/SplashScreen.swift
@@ -53,6 +53,10 @@ class SplashScreen: SKScene {
         }
     }
     
+    func fadeOutBgSound() -> Void {
+        self.ambienceSound.run(SKAction.stop())
+    }
+    
     func fadeInFadeOutLabelIntro() -> Void {
         labelIntro.run(SKAction.fadeAlpha(to: 1, duration: 3)) {
             self.labelIntro.run(SKAction.fadeAlpha(to: 0, duration: 3),completion: {
@@ -99,23 +103,44 @@ class SplashScreen: SKScene {
         
     }
     
+    private var isButtonPressed = false
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
             let touchLocation : CGPoint = touch.location(in: self)
-            
-            if startButton?.contains(touchLocation) ?? false {
-                whiteLayer?.run(SKAction.fadeAlpha(to: 1, duration: 0.3), completion: {
-                    self.whiteLayer?.run(SKAction.fadeAlpha(to: 0, duration: 1), completion: {
-                        self.overlayScreen?.run(SKAction.fadeAlpha(to: 1, duration: 2), completion: {
-                            //switch scene
-                        })
+            if !isButtonPressed {
+                if startButton?.contains(touchLocation) ?? false {
+                    isButtonPressed = true
+                    buttonPressedAction()
+
+                } else if continueButton?.contains(touchLocation) ?? false {
+                    isButtonPressed = true
+                }
+            }
+        }
+    }
+    
+    func buttonPressedAction() -> Void {
+        let actionGroup = SKAction.group([SKAction.run {
+            self.whiteLayer?.run(SKAction.fadeAlpha(to: 1, duration: 0.1), completion: {
+                self.whiteLayer?.run(SKAction.fadeAlpha(to: 0, duration: 1), completion: {
+                    self.whiteLayer?.color = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+                    self.whiteLayer?.run(SKAction.fadeAlpha(to: 1, duration: 3), completion: {
+                        self.terminateAll()
+                        self.changeScene()
                     })
                 })
-            } else if continueButton?.contains(touchLocation) ?? false {
-                
-            }
-
-        }
+            })}, SKAction.playSoundFileNamed("epic_button_pressed.mp3", waitForCompletion: false),
+                 SKAction.run { self.ambienceSound.run(SKAction.stop()) }])
+        self.run(actionGroup)
         
+    }
+    
+    func terminateAll(){
+        self.removeAllChildren()
+    }
+    
+    func changeScene(){
+        print("Change Scene")
     }
 }
