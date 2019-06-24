9b6ca3c28a1046809d408aeb6773796243138f59
Bobbyphtr <bobbypehtrus2@gmail.com>
2019-06-24 23:49:13 +0700

    Ending Scene Done...

--- Blackpage/Controllers/EndingScene.swift
+++ Blackpage/Controllers/EndingScene.swift
@@ -20,23 +20,50 @@ class EndingScene: SKScene {
     override func didMove(to view: SKView) {
         
         initiateComponents()
-        
+        self.run(SKAction.run(fadeInFadeOutLabelEnding_1))
     }
     
     func initiateComponents(){
-        
+        blackscreen = childNode(withName: "blackscreen") as? SKSpriteNode
+        label1 = childNode(withName: "Label 1") as? SKLabelNode
+        label2 = childNode(withName: "Label 2") as? SKLabelNode
+        label3 = childNode(withName: "Label 3") as? SKLabelNode
     }
     
     override func update(_ currentTime: TimeInterval) {
-        // yes
         
     }
-    
-    private var isButtonPressed = false
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
     }
     
-   
+    func fadeInFadeOutLabelEnding_1() -> Void {
+        label1?.run(SKAction.fadeAlpha(to: 1, duration: 3)) {
+            self.label1?.run(SKAction.fadeAlpha(to: 0, duration: 5),completion: {
+                self.label1?.removeFromParent()
+                self.fadeInFadeOutLabelEnding_2()
+            })
+        }
+    }
+    func fadeInFadeOutLabelEnding_2() -> Void {
+        label2?.run(SKAction.fadeAlpha(to: 1, duration: 3)) {
+            self.label2?.run(SKAction.fadeAlpha(to: 0, duration: 5),completion: {
+                self.label2?.removeFromParent()
+                self.fadeInFadeOutLabelEnding_3()
+            })
+        }
+    }
+    func fadeInFadeOutLabelEnding_3() -> Void {
+        label3?.run(SKAction.fadeAlpha(to: 1, duration: 3)) {
+            self.label3?.run(SKAction.fadeAlpha(to: 0, duration: 5),completion: {
+                self.label3?.removeFromParent()
+                self.backToSplashScreen()
+            })
+        }
+    }
+    
+    func backToSplashScreen(){
+        
+    }
 }
