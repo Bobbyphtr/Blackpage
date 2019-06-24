4ec2dc1a6cc54b0095270c9227d357ff7409a0f0
Bobbyphtr <bobbypehtrus2@gmail.com>
2019-06-24 17:37:37 +0700

    Revert "Merge branch 'Bobby-backdoorEnding'"
    This reverts commit 18972c9f3832c7655b4d8386a4ef96a5851a139e.

--- Blackpage/Controllers/SplashScreen.swift
+++ Blackpage/Controllers/SplashScreen.swift
@@ -99,14 +99,8 @@ class SplashScreen: SKScene {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
       for touch in touches {
             let touchLocation : CGPoint = touch.location(in: self)
-            if !isButtonPressed {
-                if startButton?.contains(touchLocation) ?? false {
-                    isButtonPressed = true
-                    buttonPressedAction() 
-
-                } else if continueButton?.contains(touchLocation) ?? false {
-                    isButtonPressed = true
-                }
+            if startButton?.contains(touchLocation) ?? false {
+                //self.scene?.view?.presentScene(SKScene.init(fileNamed: "OpeningScene")!, transition: SKTransition.fade(withDuration: 3.0))
             }
 
         }
