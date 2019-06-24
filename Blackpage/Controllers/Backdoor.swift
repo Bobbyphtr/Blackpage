f9a53bc59cf27f525604e22e3e1fd6fddcf66cba
Bobbyphtr <bobbypehtrus2@gmail.com>
2019-06-24 19:40:07 +0700

    added kitchen backdoor

---
+++ Blackpage/Controllers/Backdoor.swift
@@ -0,0 +1,14 @@
+//
+//  FrontDoor.swift
+//  Blackpage
+//
+//  Created by BobbyPhtr on 24/06/19.
+//  Copyright © 2019 BobbyPhtr. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class Backdoor: SKScene {
+    
+}
