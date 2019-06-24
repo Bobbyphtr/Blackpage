35ec80de936e3f8b294823d90dcadfe2223f3282
Bobbyphtr <bobbypehtrus2@gmail.com>
2019-06-24 15:20:27 +0700

    Ending on the go

---
+++ Blackpage/Controllers/EndingScene.swift
@@ -0,0 +1,36 @@
+//
+//  EndingScene.swift
+//  Blackpage
+//
+//  Created by BobbyPhtr on 24/06/19.
+//  Copyright © 2019 BobbyPhtr. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class EndingScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+        initiateComponents()
+        
+    }
+    
+    func initiateComponents(){
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+        
+    }
+    
+    private var isButtonPressed = false
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+   
+}
