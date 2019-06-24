d5c6069ae6fab9baf917c77ab8e4f5e324cd0bf7
Bobbyphtr <bobbypehtrus2@gmail.com>
2019-06-24 23:51:01 +0700

    GameViewController change to SplashScreen fix

--- Blackpage/GameViewController.swift
+++ Blackpage/GameViewController.swift
@@ -16,7 +16,7 @@ class GameViewController: UIViewController {
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
-            if let scene = EndingScene(fileNamed: "EndingScene") {
+            if let scene = SplashScreen(fileNamed: "SplashScreen") {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
                 
